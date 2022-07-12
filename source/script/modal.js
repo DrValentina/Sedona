c65355224464f0aa53145d6ce28852943f9669ca
DrValentina <valya_dragunova@mail.ru>
2022-07-12 19:38:47 +0300

    js для модалки index

---
+++ source/script/modal.js
@@ -0,0 +1,6 @@
+let modal = document.querySelector(".modal");
+let modalButton = document.querySelector(".map__button");
+
+modalButton.addEventListener("click", function() {
+  modal.classList.toggle("visually-hidden");
+})
